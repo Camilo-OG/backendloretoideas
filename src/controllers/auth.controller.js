22a773a3a261085d76d33b9338d1dcb935c3ce4f
Annydrn <andrea.duran.cabana@gmail.com>
2023-08-09 18:58:13 -0400

    .5

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const userModel= require("../schemas/user.model")
 
-
-exports.register=async (req,res)=>{
-try {
-    const userlist= await userModel.find()
-    res.status(200).json(userlist)
-} catch (error) {
-    console.log("saludo desde el registro")
-    res.status(400).json({msg: error})
-}
-}
+exports.register= async(req,res)=>{{res.send({msg: "saludo desde el register"})}};
  exports.login= async(req,res)=>{{res.send({msg: "saludo desde el login"}
  )}}
 
