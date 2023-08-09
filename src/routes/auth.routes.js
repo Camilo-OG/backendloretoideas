df84c33a099198d5a5fd27c373c59f40d685a369
Annydrn <andrea.duran.cabana@gmail.com>
2023-08-09 15:51:41 -0400

    creacion de controlador usuarios

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const register= require("../controllers/auth.controller.js")
+const login= require("../controllers/auth.controller.js")
 
-router.post("/register")
-router.post("/login")
+
+router.post("/register", register)
+router.post("/login", login)
 
 
+module.exports= router
