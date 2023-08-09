a92e6ee6ebf93eb95a4133d0ee6992fce84ff8d0
Annydrn <andrea.duran.cabana@gmail.com>
2023-08-09 16:29:31 -0400

    actualizacion register

--- src/models/auth.controller.js
+++ src/models/auth.controller.js
@@ -2,9 +2,16 @@ const express = require('express');
 const userModel= require("../schemas/user.model")
 
 
-const register= (req,res)=>{res.send({msg: "saludo desde el registro"}
-    )}
- const login= (req,res)=>{{res.send({msg: "saludo desde el login"}
+const register=async (req,res)=>{
+try {
+    const userlist= await userModel.find()
+    res.status(200).json(userlist)
+} catch (error) {
+    console.log(error)
+    res.status(400).json({msg: error})
+}
+}
+ const login= async(req,res)=>{{res.send({msg: "saludo desde el login"}
  )}}
 
  module.exports={register,login}
