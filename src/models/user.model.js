d4654be37305f2a4ea413677e6e7ff9f797d010e
Annydrn <andrea.duran.cabana@gmail.com>
2023-08-09 12:38:50 -0400

    creando base de datos login

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema= mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+export default mongoose.model("User_login",userSchema);
