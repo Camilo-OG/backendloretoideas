d7ffcd28fb4de46524539d4792638a7dc881de1a
Annydrn <andrea.duran.cabana@gmail.com>
2023-08-13 16:08:19 -0400

    arreglado problema con token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -2,8 +2,7 @@ const jwt = require("jsonwebtoken");
 const tokenSecret = process.env.TOKEN_SECRET;
 
 exports.authRequired = (req, res, next) => {
-  const token = req.cookies;
-  console.log(user);
+  const {token} = req.cookies;
    
   if (!token)
     return res
@@ -11,9 +10,10 @@ exports.authRequired = (req, res, next) => {
       .json({ message: "No hay token , ingreso no autorizado" });
 
   jwt.verify(token, tokenSecret, (err, user) => {
-    
-    if (err) return res.status(403).json({ message: "token invalido" });
+ 
 
+    if (err) res.status(403).json({ message: "token invalido" });
+    console.log(user);
     next();
   });
 };
