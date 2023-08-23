33d4b14588f2cd15a1a8d0b2ba479bead2536f03
camilo-OG <camilo.osorio.gsach.cl>
2023-08-23 17:12:50 -0400

    actualizacion cors para netlify

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://64e65fcdd67c7802d9873f97--lustrous-starship-9bfc30.netlify.app"
+    "https://64e6760743746206779281b7--lustrous-starship-9bfc30.netlify.app"
   ],
   methods: ["GET", "POST", "PUT"], // Métodos permitidos
   allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
